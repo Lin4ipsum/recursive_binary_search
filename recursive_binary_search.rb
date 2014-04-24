3d43a75501e5f727e9f526522579ace108cc32ec
Lin4ipsum <lin.m.gonzalez@gmail.com>
2014-04-23 22:49:03 -0400

    add spec to return value if in array

---
+++ recursive_binary_search.rb
@@ -0,0 +1,18 @@
+class Search
+
+	def recursive_binary_search(array, value, from=0, to=nil)
+		if to == nil
+    	to = array.length - 1
+    end
+ 
+    mid = (from + to) / 2
+ 
+    if value < array[mid]
+        return recursive_binary_search array, value, from, mid - 1
+    elsif value > array[mid]
+        return recursive_binary_search array, value, mid + 1, to
+    else
+        return array[mid]
+    end
+	end
+end
---
+++ recursive_binary_search_spec.rb
@@ -0,0 +1,18 @@
+require_relative "recursive_binary_search"
+require 'rspec'
+
+describe Search do
+	before do
+		@s = Search.new
+	end
+
+	it "will default from to 0 and to nil if not specified" do
+		a = [3,5,17,22,31,53,77,92]
+		expect(@s.recursive_binary_search(a, 17)).to_not raise_error(ArgumentError)
+	end
+
+	it "will return the value if the value is in the arrray" do
+		a = [3,5,17,22,31,53,77,92]
+		@s.recursive_binary_search(a, 17).should == 17
+	end
+end
