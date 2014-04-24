bc42a2f85be7d7f3880471117e61a9a2008f914f
Lin4ipsum <lin.m.gonzalez@gmail.com>
2014-04-23 22:54:32 -0400

    add spec and code for when value not in array

--- recursive_binary_search_spec.rb
+++ recursive_binary_search_spec.rb
@@ -15,4 +15,9 @@ describe Search do
 		a = [3,5,17,22,31,53,77,92]
 		@s.recursive_binary_search(a, 17).should == 17
 	end
+
+	it "will not return the value if the value is not in the arrray" do
+		a = [3,5,17,22,31,53,77,92]
+		@s.recursive_binary_search(a, 18).should == nil
+	end
 end
